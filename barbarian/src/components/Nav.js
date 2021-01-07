a228ccac70b3f1b12de3d6ccfe6d7db2509face2
McKay Nelson <tmckaynelson@gmail.com>
2021-01-06 17:26:45 -0700

    basic nav bar structure made

--- barbarian/src/components/Nav.js
+++ barbarian/src/components/Nav.js
@@ -3,7 +3,13 @@ import './Nav.css'
 export default function Nav() {
     return (
         <div className="nav">
-            <h1>nav bar</h1>
+            <img src="../cropped-Barbarian-Title-with-Shadow-1.png"/>
+            <ul className="nav-links">
+                <li>About the Author</li>
+                <li>Barbarian Home</li>
+                <li>Contact Me</li>
+                <li>Sample Chapter</li>
+            </ul>
         </div>
     )
 }
